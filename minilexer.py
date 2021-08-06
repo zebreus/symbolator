ba8c922b59280d9ed8467c3ed33e197688659ac3
Wouter van Verre <woutervanverre@gmail.com>
2021-08-06 17:00:13 +0100

    Change the formatting with PyCharm
    Signed-off-by: Wouter van Verre <woutervanverre@gmail.com>

--- minilexer.py
+++ minilexer.py
@@ -5,86 +5,85 @@ from __future__ import print_function
 
 import re
 
-'''Minimalistic lexer engine inspired by the PyPigments RegexLexer'''
+"""Minimalistic lexer engine inspired by the PyPigments RegexLexer"""
 
 __version__ = '1.0.7'
 
+
 class MiniLexer(object):
-  '''Simple lexer state machine with regex matching rules'''
+    """Simple lexer state machine with regex matching rules"""
 
-  def __init__(self, tokens, flags=re.MULTILINE):
-    '''Create a new lexer
-    
-    Args:
-      tokens (dict(match rules)): Hierarchical dict of states with a list of regex patterns and transitions
-      flags (int): Optional regex flags
-    '''
-    self.tokens = {}
+    def __init__(self, tokens, flags=re.MULTILINE):
+        """Create a new lexer
 
-    # Pre-process the state definitions
-    for state, patterns in tokens.items():
-      full_patterns = []
-      for p in patterns:
-        pat = re.compile(p[0], flags)
-        action = p[1]
-        new_state = p[2] if len(p) >= 3 else None
+        Args:
+          tokens (dict(match rules)): Hierarchical dict of states with a list of regex patterns and transitions
+          flags (int): Optional regex flags
+        """
+        self.tokens = {}
 
-        # Convert pops into an integer
-        if new_state and new_state.startswith('#pop'):
-          try:
-            new_state = -int(new_state.split(':')[1])
-          except ValueError:
-            new_state = -1
-          except IndexError:
-            new_state = -1
+        # Pre-process the state definitions
+        for state, patterns in tokens.items():
+            full_patterns = []
+            for p in patterns:
+                pat = re.compile(p[0], flags)
+                action = p[1]
+                new_state = p[2] if len(p) >= 3 else None
 
-        full_patterns.append((pat, action, new_state))
-      self.tokens[state] = full_patterns
+                # Convert pops into an integer
+                if new_state and new_state.startswith('#pop'):
+                    try:
+                        new_state = -int(new_state.split(':')[1])
+                    except ValueError:
+                        new_state = -1
+                    except IndexError:
+                        new_state = -1
 
+                full_patterns.append((pat, action, new_state))
+            self.tokens[state] = full_patterns
 
-  def run(self, text):
-    '''Run lexer rules against a source text
+    def run(self, text):
+        """Run lexer rules against a source text
 
-    Args:
-      text (str): Text to apply lexer to
+        Args:
+          text (str): Text to apply lexer to
 
-    Yields:
-      A sequence of lexer matches.
-    '''
+        Yields:
+          A sequence of lexer matches.
+        """
 
-    stack = ['root']
-    pos = 0
+        stack = ['root']
+        pos = 0
 
-    patterns = self.tokens[stack[-1]]
+        patterns = self.tokens[stack[-1]]
 
-    while True:
-      for pat, action, new_state in patterns:
-        m = pat.match(text, pos)
-        if m:
-          if action:
-            #print('## MATCH: {} -> {}'.format(m.group(), action))
-            #print(m.string[m.pos:m.endpos])
-            yield (pos, m.end()-1), action, m.groups()
+        while True:
+            for pat, action, new_state in patterns:
+                m = pat.match(text, pos)
+                if m:
+                    if action:
+                        # print('## MATCH: {} -> {}'.format(m.group(), action))
+                        # print(m.string[m.pos:m.endpos])
+                        yield (pos, m.end() - 1), action, m.groups()
 
-          pos = m.end()
+                    pos = m.end()
 
-          if new_state:
-            if isinstance(new_state, int): # Pop states
-              del stack[new_state:]
+                    if new_state:
+                        if isinstance(new_state, int):  # Pop states
+                            del stack[new_state:]
+                        else:
+                            stack.append(new_state)
+
+                        # print('## CHANGE STATE:', pos, new_state, stack)
+                        patterns = self.tokens[stack[-1]]
+
+                    break
+
             else:
-              stack.append(new_state)
-
-            #print('## CHANGE STATE:', pos, new_state, stack)
-            patterns = self.tokens[stack[-1]]
-
-          break
-
-      else:
-        try:
-          if text[pos] == '\n':
-            pos += 1
-            continue
-          pos += 1
-        except IndexError:
-          break
-
+                try:
+                    if text[pos] == '\n':
+                        pos += 1
+                        continue
+                    pos += 1
+                except IndexError:
+                    break
