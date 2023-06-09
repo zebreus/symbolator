9a7f322169b95b6b7dc7da6161a2c303faf5cbb5
Lennart <lennarteichhorn@googlemail.com>
2023-06-09 00:20:59 +0000

    Integrate hdlparse

--- flake.nix
+++ flake.nix
@@ -8,9 +8,45 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      rec {
+      let
+        composeExtensions =
+          f: g: self: super:
+          let
+            fApplied = f self super;
+            super' = super // fApplied;
+          in
+          fApplied // g self super';
+        pkgs = nixpkgs.legacyPackages.${system};
+        packageOverrides = pkgs.callPackage ./python-packages.nix { };
+        pythonPackagesLocalOverrides = self: super: with pkgs; {
+          symbolator = super.symbolator.override (attrs: {
+            nativeBuildInputs = [ wrapGAppsHook gobject-introspection ] ++ attrs.nativeBuildInputs;
+
+            propagatedBuildInputs = [
+              pango
+              fontconfig
+              gobject-introspection
+              python3Packages.pygobject3
+            ] ++ attrs.propagatedBuildInputs;
+
+            meta = with lib; {
+              description = "A component diagramming tool for VHDL and Verilog";
+              longDescription = ''
+                Symbolator is a component diagramming tool for VHDL and Verilog. It will parse HDL source files, extract components or modules and render them as an image.
+              '';
+              homepage = "https://hdl.github.io/symbolator/";
+              license = licenses.mit;
+              platforms = lib.platforms.linux;
+              mainProgram = "symbolator";
+            };
+          });
+        };
+        pythonPackages = pkgs.python3.override { packageOverrides = composeExtensions packageOverrides pythonPackagesLocalOverrides; }.pkgs;
+      in
+      rec
+      {
         name = "Symbolator";
-        packages.symbolator = import ./default.nix { pkgs = nixpkgs.legacyPackages.${system}; };
+        packages.symbolator = pythonPackages.symbolator;
         packages.default = packages.symbolator;
       }
     );
