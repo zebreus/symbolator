3684991e70fc68aaf664e25a853107c82567e272
Lennart <lennarteichhorn@googlemail.com>
2023-06-10 15:44:13 +0000

    Release v1.2.0

--- python-packages.nix
+++ python-packages.nix
@@ -326,7 +326,7 @@ self: super: {
   };
   "symbolator" = super.buildPythonPackage rec {
     pname = "symbolator";
-    version = "1.1.1";
+    version = "1.2.0";
     src = ./.;
     format = "setuptools";
     doCheck = false;
